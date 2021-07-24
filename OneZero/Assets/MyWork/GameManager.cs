76f43ccd22206480b13c7693f6774c5baa2a2075
김예리나 <ggm.s200001@ggh.goe.go.kr>
2021-07-24 23:18:00 +0900

    와 존나어려웠다;; 암튼 풀링함

---
+++ OneZero/Assets/MyWork/GameManager.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public GameObject note = null;
+
+    public List<GameObject> MobPool = new List<GameObject>();
+    public GameObject[] Mobs;
+    public int objCnt = 1;
+
+    GameObject CreateObj(GameObject obj, Transform parent)
+    {
+        GameObject copy = Instantiate(obj);
+        copy.transform.SetParent(parent);
+        copy.SetActive(false);
+        return copy;
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        //StartCoroutine(SpawnNoteOne());
+        StartCoroutine(CreateMob());
+    }
+
+    void Awake()
+    {
+        for (int i = 0; i < Mobs.Length; i++)
+        {
+            for (int q = 0; q < objCnt; q++)
+            {
+                MobPool.Add(CreateObj(Mobs[i], transform));
+            }
+        }
+    }
+    private IEnumerator SpawnNoteOne()
+    {
+        while(true)
+        {
+            float randomX = Random.Range(-10, 10);
+            Instantiate(note, new Vector2(randomX, 2), Quaternion.identity);
+            yield return new WaitForSeconds(0.2f);
+            float delay = 2;
+            yield return new WaitForSeconds(delay);
+        }
+        
+    }
+
+    IEnumerator CreateMob()
+    {
+        while (true)
+        {
+            MobPool[Random.Range(0, MobPool.Count)].SetActive(true);
+            yield return new WaitForSeconds(Random.Range(1.3f, 4f));
+        }
+    }
+}
---
+++ OneZero/Assets/MyWork/Note.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Note : MonoBehaviour
+{
+    public float mobSpeed = 0;
+    public Vector2 StartPosition;
+    public void OnEnable()
+    {
+        transform.position = StartPosition;
+    }
+    // Start is called before the first frame update
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector2.left * Time.deltaTime * mobSpeed);
+
+        if (transform.position.x < -10)
+        {
+            gameObject.SetActive(false);
+        }
+    }
+}
