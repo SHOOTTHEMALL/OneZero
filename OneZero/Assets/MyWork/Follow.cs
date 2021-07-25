f8f4e6c8a8451bb249bf099187d2278e66155c0e
김예리나 <ggm.s200001@ggh.goe.go.kr>
2021-07-25 16:12:18 +0900

    이동

---
+++ OneZero/Assets/MyWork/Follow.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class Follow : MonoBehaviour
+{
+    private void Awake()
+    {
+        transform.DOMoveX(0.73f, 2);
+    }
+}
---
+++ OneZero/Assets/MyWork/MoveThem.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class MoveThem : MonoBehaviour
+{
+    private void Awake()
+    {
+        transform.DOMoveX(-1.3f, 5);
+    }
+}
