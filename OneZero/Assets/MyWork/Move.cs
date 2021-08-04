d1c0e73b50c739d2f5436468ad8ad9dc95434e14
김예리나 <ggm.s200001@ggh.goe.go.kr>
2021-08-04 11:20:16 +0900

    대화 이동 애니메이션

---
+++ OneZero/Assets/MyWork/Move.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using DG.Tweening;
+
+public class Move : MonoBehaviour
+{
+    private void Update()
+    {
+        if(gameObject.CompareTag("One"))
+        {
+            transform.DOMoveX(7, 2);
+        }
+        else if(gameObject.CompareTag("Zero"))
+        {
+            transform.DOMoveX(-6.5f, 2);
+        }
+        else if (gameObject.CompareTag("Up"))
+        {
+            transform.DOMoveY(-3.3f, 2);
+        }
+    }
+}
