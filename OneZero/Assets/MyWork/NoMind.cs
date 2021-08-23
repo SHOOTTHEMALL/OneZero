b121361d0fa93e6568aafc5c1b1d14337d7a3daf
김예리나 <ggm.s200001@ggh.goe.go.kr>
2021-08-24 00:36:58 +0900

    나도 날 줄테니 너도 날 줘라

--- OneZero/Assets/MyWork/NoMind.cs
+++ OneZero/Assets/MyWork/NoMind.cs
@@ -4,27 +4,43 @@ using UnityEngine;
 using UnityEngine.EventSystems;
 
 
-public class NoMind : MonoBehaviour, IBeginDragHandler, IEndDragHandler, IDragHandler
+public class NoMind : Slot, IBeginDragHandler, IEndDragHandler, IDragHandler 
 {
     public static Vector2 defaultposition;//드롭하면 다시 원위치로 보내기위한 변수 void Start () { } void Update () { } }
+
+    [SerializeField] private Canvas canvas;
+
+    private RectTransform moveFuck;
+    private CanvasGroup canFuck;
+
+    private void Awake()
+    {
+        moveFuck = GetComponent<RectTransform>();
+        canFuck = GetComponent<CanvasGroup>();
+    }
 
     public void OnBeginDrag(PointerEventData eventData)
     {
         //throw new System.NotImplementedException(); 이게 뭐지?
         defaultposition = transform.position;
+        canFuck.blocksRaycasts = false;
+        canFuck.alpha = .6f;
     }
 
     public void OnDrag(PointerEventData eventData)
     {
-        //throw new System.NotImplementedException();
-        Vector2 currentPos = Input.mousePosition; 
-        transform.position = currentPos;
+        moveFuck.anchoredPosition += eventData.delta / canvas.scaleFactor;
     }
-
     public void OnEndDrag(PointerEventData eventData)
     {
         //throw new System.NotImplementedException();
-        Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition); 
-        transform.position = mousePos;
+        Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        transform.position = defaultposition;
+        canFuck.blocksRaycasts = true;
+        canFuck.alpha = 1f;
+        //if (eventData.pointerDrag != null)
+        //{
+        //    transform.position = mousePos; 핸드폰에서 해야한다. 
+        //}
     }
 }
---
+++ OneZero/Assets/MyWork/Slot.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class Slot : MonoBehaviour , IDropHandler 
+{
+    public void OnDrop(PointerEventData eventData)
+    {
+        //throw new System.NotImplementedException();
+        if (eventData.pointerDrag != null)
+        {
+            //eventData.pointerDrag.GetComponent<RectTransform>().anchoredPosition = GetComponent<RectTransform>().anchoredPosition; //현재 위치를 먹어버림.
+            
+            Debug.Log("왜안되노 이거");
+        }
+    }
+}
