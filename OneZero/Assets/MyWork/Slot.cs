ae575822bb5e676cd23118e4e8c5540e69b0cfa9
김예리나 <ggm.s200001@ggh.goe.go.kr>
2021-09-01 20:22:22 +0900

    ㅇㅇㅇ

--- OneZero/Assets/MyWork/Slot.cs
+++ OneZero/Assets/MyWork/Slot.cs
@@ -9,14 +9,11 @@ public class Slot : MonoBehaviour , IDropHandler
     public void OnDrop(PointerEventData eventData)
     {
         //throw new System.NotImplementedException();
-        if (eventData.pointerDrag != null)
+        if (eventData.pointerDrag != null && gameObject.CompareTag("iP"))
         {
-            if(gameObject.CompareTag("iP"))
-            {
                 eventData.pointerDrag.GetComponent<RectTransform>().anchoredPosition = GetComponent<RectTransform>().anchoredPosition; //현재 위치를 먹어버림.
                 Naruto = true;
                 //eventData.pointerDrag.gameObject.transform.position = gameObject.transform.position;
-            }
         }
         else
             Naruto = false;
