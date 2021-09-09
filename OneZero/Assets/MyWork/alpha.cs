972e870c1aaa57b41e37ca2939a564eee03a8bcc
김예리나 <ggm.s200001@ggh.goe.go.kr>
2021-09-10 00:52:11 +0900

    https://app.diagrams.net/

---
+++ OneZero/Assets/MyWork/alpha.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class alpha : MonoBehaviour
+{
+    public CanvasGroup alpa;
+    private float adidas = 0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        adidas += Time.deltaTime;
+        if(adidas>4)
+        {
+            alpa.alpha += 0.01f;
+           // adidas = 0;
+        }
+    }
+}
