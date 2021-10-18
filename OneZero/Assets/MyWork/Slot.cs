0f88ede8c8e2e0b9e1d3f76d61686380818ecc93
김예리나 <ggm.s200001@ggh.goe.go.kr>
2021-10-19 01:55:53 +0900

    망했어그냥

--- OneZero/Assets/MyWork/Slot.cs
+++ OneZero/Assets/MyWork/Slot.cs
@@ -2,17 +2,38 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
 public class Slot : MonoBehaviour , IDropHandler
 {
+    public Image clothes;
+    private List<Image> clothe = new List<Image>();
+
+    public SpriteRenderer srS;
+    private List<SpriteRenderer> sr = new List<SpriteRenderer>();
+
+    public void Awake()
+    {
+        clothes = GameObject.Find("Meow").GetComponent<Image>();
+        //clothes.GetComponentInChildren<Image>(clothe);
+        clothe.RemoveAt(0);
+
+        srS = GameObject.Find("clothes").GetComponent<SpriteRenderer>();
+        //srS.GetComponentInChildren<SpriteRenderer>(sr);
+        sr.RemoveAt(0);
+    }
     public void OnDrop(PointerEventData eventData)
     {
-        //throw new System.NotImplementedException();
-        if (eventData.pointerDrag != null)//eventData.pointerDrag != null 의 조건은 한번 들어가면 다시 초기화가 안된다. 어카지
+        if(eventData.pointerDrag == clothe[0])
         {
-            //eventData.pointerDrag.GetComponent<RectTransform>().anchoredPosition = GetComponent<RectTransform>().anchoredPosition;//현재 위치를 먹어버림.
-            eventData.pointerDrag.gameObject.transform.position = gameObject.transform.position;
+            
         }
+        //throw new System.NotImplementedException();
+        //if (eventData.pointerDrag != null)//eventData.pointerDrag != null 의 조건은 한번 들어가면 다시 초기화가 안된다. 어카지
+        //{
+        //    //eventData.pointerDrag.GetComponent<RectTransform>().anchoredPosition = GetComponent<RectTransform>().anchoredPosition;//현재 위치를 먹어버림.
+        //    eventData.pointerDrag.gameObject.transform.position = gameObject.transform.position;
+        //}
         //else if(eventData.pointerDrag != null && gameObject.CompareTag("pI"))
         //{
         //    NoMind.defaultposition = transform.position;
